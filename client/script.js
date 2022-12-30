3d06036f9e9fcb911e34f5c964dd4bacd9067c4d
yourfriend <81387799+yourfriendoss@users.noreply.github.com>
2022-12-30 22:52:39 +0200

    Siteban _id Mismatch

--- client/script.js
+++ client/script.js
@@ -2991,7 +2991,7 @@ $(function () {
     function submit() {
       var msg = { m: "siteban" };
 
-      msg.id = $("#siteban .text[name=id]").val();
+      msg._id = $("#siteban .text[name=id]").val();
 
       var durationUnit = $("#siteban select[name=durationUnit]").val();
       if (durationUnit === "permanent") {
